1f6e36ca84ad4cf6f71fff45dfe3db09de3e29af
Joshua David <dazlej4god@gmail.com>
2020-02-26 14:06:24 +0100

    contact form

--- dist/js/main.js
+++ dist/js/main.js
@@ -31,3 +31,71 @@ function toggleMenu() {
     showMenu = false;
   }
 }
+
+// Questions Array
+const questions = [
+  { question: "Enter Your First Name" },
+  { question: "Enter Your Last Name" },
+  { question: "Enter Your Email", pattern: /\S+@\S+\.\S+/ },
+  { question: "Create A Password", type: "password" }
+];
+
+// Transition Times
+const shakeTime = 100; // Shake Transition Time
+const switchTime = 200; // Transition Between Questions
+
+// Init Position At First Question
+let position = 0;
+
+// Init DOM Elements
+const formBox = document.querySelector("#form-box");
+const nextBtn = document.querySelector("#next-btn");
+const prevBtn = document.querySelector("#prev-btn");
+const inputGroup = document.querySelector("#input-group");
+const inputField = document.querySelector("#input-field");
+const inputLabel = document.querySelector("#input-label");
+const inputProgress = document.querySelector("#input-progress");
+const progress = document.querySelector("#progress-bar");
+
+// Events
+
+// Get Questions on DOM load
+document.addEventListener("DOMContentLoaded", getQuestion);
+
+// Functions
+
+// Get Question from Array & Add to Markup
+function getQuestion() {
+  // Get Current Question
+  inputLabel.innerHTML = questions[position].question;
+  // Get Current Type
+  inputField.type = questions[position].type || "text";
+  // Get Current Answer
+  inputField.value - questions[positions].answer || " ";
+  // Focus On Element
+  inputField.focus();
+
+  // Set Progress Bar Width - Variable to the questions length
+  progress.style.width = (position * 100) / questions.length + "%";
+
+  // Add User Icon or Back Arrow depending on question
+  prevBtn.className = position ? "fas fa-arrow-left" : "fas fa-user";
+
+  showQuestion();
+}
+
+// Display Question To User
+function showQuestion() {
+  inputGroup.style.opacity = 1;
+  inputProgress.style.transition = "";
+  inputProgress.style.width = "100%";
+}
+
+// Hide Question From User
+function hideQuestion() {
+  inputGroup.style.opacity = 0;
+  inputLabel.style.marginLeft = 0;
+  inputProgress.style.width = 0;
+  inputProgress.style.transition = "none";
+  inputGroup.style.border = null;
+}
