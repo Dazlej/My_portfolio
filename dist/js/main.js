93d07e04a275248cb6b427f6fc919b7ac3aa8a79
Joshua David <dazlej4god@gmail.com>
2020-03-01 15:54:14 +0100

    Completion of Contact form

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,101 +1,33 @@
 // Select DOM Items
-const menuBtn = document.querySelector(".menu-btn");
-const menu = document.querySelector(".menu");
-const menuNav = document.querySelector(".menu-nav");
-const menuBranding = document.querySelector(".menu-branding");
-const navItems = document.querySelectorAll(".nav-item");
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-item');
 
 // Set Initial State Of Menu
 let showMenu = false;
 
-menuBtn.addEventListener("click", toggleMenu);
+menuBtn.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
   if (!showMenu) {
-    menuBtn.classList.add("close");
-    menu.classList.add("show");
-    menuNav.classList.add("show");
-    menuBranding.classList.add("show");
-    navItems.forEach(item => item.classList.add("show"));
+    menuBtn.classList.add('close');
+    menu.classList.add('show');
+    menuNav.classList.add('show');
+    menuBranding.classList.add('show');
+    navItems.forEach(item => item.classList.add('show'));
 
     // Set Menu State
     showMenu = true;
   } else {
-    menuBtn.classList.remove("close");
-    menu.classList.remove("show");
-    menuNav.classList.remove("show");
-    menuBranding.classList.remove("show");
-    navItems.forEach(item => item.classList.remove("show"));
+    menuBtn.classList.remove('close');
+    menu.classList.remove('show');
+    menuNav.classList.remove('show');
+    menuBranding.classList.remove('show');
+    navItems.forEach(item => item.classList.remove('show'));
 
     // Set Menu State
     showMenu = false;
   }
-}
-
-// Questions Array
-const questions = [
-  { question: "Enter Your First Name" },
-  { question: "Enter Your Last Name" },
-  { question: "Enter Your Email", pattern: /\S+@\S+\.\S+/ },
-  { question: "Create A Password", type: "password" }
-];
-
-// Transition Times
-const shakeTime = 100; // Shake Transition Time
-const switchTime = 200; // Transition Between Questions
-
-// Init Position At First Question
-let position = 0;
-
-// Init DOM Elements
-const formBox = document.querySelector("#form-box");
-const nextBtn = document.querySelector("#next-btn");
-const prevBtn = document.querySelector("#prev-btn");
-const inputGroup = document.querySelector("#input-group");
-const inputField = document.querySelector("#input-field");
-const inputLabel = document.querySelector("#input-label");
-const inputProgress = document.querySelector("#input-progress");
-const progress = document.querySelector("#progress-bar");
-
-// Events
-
-// Get Questions on DOM load
-document.addEventListener("DOMContentLoaded", getQuestion);
-
-// Functions
-
-// Get Question from Array & Add to Markup
-function getQuestion() {
-  // Get Current Question
-  inputLabel.innerHTML = questions[position].question;
-  // Get Current Type
-  inputField.type = questions[position].type || "text";
-  // Get Current Answer
-  inputField.value - questions[positions].answer || " ";
-  // Focus On Element
-  inputField.focus();
-
-  // Set Progress Bar Width - Variable to the questions length
-  progress.style.width = (position * 100) / questions.length + "%";
-
-  // Add User Icon or Back Arrow depending on question
-  prevBtn.className = position ? "fas fa-arrow-left" : "fas fa-user";
-
-  showQuestion();
-}
-
-// Display Question To User
-function showQuestion() {
-  inputGroup.style.opacity = 1;
-  inputProgress.style.transition = "";
-  inputProgress.style.width = "100%";
-}
-
-// Hide Question From User
-function hideQuestion() {
-  inputGroup.style.opacity = 0;
-  inputLabel.style.marginLeft = 0;
-  inputProgress.style.width = 0;
-  inputProgress.style.transition = "none";
-  inputGroup.style.border = null;
 }
